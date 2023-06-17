84b23e9544099c30a9173e2614f06a00ed184f65
DanRuizK57 <d.ruizk03@ufromail.cl>
2023-06-17 17:05:59 -0400

    Validaciones añadidas a las funciones del controlador

--- src/controllers/calculadora.controller.js
+++ src/controllers/calculadora.controller.js
@@ -28,7 +28,14 @@ function restar(req, res){
 
     const result = number1 - number2;
 
-    res.send({ result })
+    if (isNaN(number1)) {
+        res.status(400).send("ERROR: number1 debe ser un número!")
+    } else if (isNaN(number2)) {
+        res.status(400).send("ERROR: number2 debe ser un número!")
+    }
+    else {
+        res.send({ result })
+    }
 }
 
 function multiplicar(req, res){
@@ -37,16 +44,32 @@ function multiplicar(req, res){
 
     const result = number1 * number2;
 
-    res.send({ result })
+    if (isNaN(number1)) {
+        res.status(400).send("ERROR: number1 debe ser un número!")
+    } else if (isNaN(number2)) {
+        res.status(400).send("ERROR: number2 debe ser un número!")
+    }
+    else {
+        res.send({ result })
+    }
 }
 
 function dividir(req, res){
-    const number1 = req.body.number1;
-    const number2 = req.body.number2;
+    const quotient = req.body.quotient;
+    const divisor = req.body.divisor;
 
-    const result = number1 / number2;
+    const result = quotient / divisor;
 
-    res.send({ result })
+    if (isNaN(quotient)) {
+        res.status(400).send("ERROR: quotient debe ser un número!")
+    } else if (isNaN(divisor)) {
+        res.status(400).send("ERROR: divisor debe ser un número!")
+    } else if (divisor === 0) {
+        res.status(400).send("ERROR: divisor no puede ser 0!")
+    }
+    else {
+        res.send({ result })
+    }
 }
 
 export { getDescription, sumar, restar, multiplicar, dividir };
